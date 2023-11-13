600e83f647217324845d0d97a2191f20960be675
connor <47131276+Yodude2002@users.noreply.github.com>
2023-11-13 15:58:01 -0500

    added a pause menu

---
+++ scripts/level_base.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if Input.is_action_just_pressed("pause"):
+		var child = preload("res://scenes/levels/pause_menu.tscn").instantiate()
+		child.position = self.get_window().size / 2
+		self.add_child(child)
