9bf7692864b9f917ebbb952a8b0b14ee10bfa017
connor <47131276+Yodude2002@users.noreply.github.com>
2023-11-15 16:15:22 -0500

    ESC will now unpause the game

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -8,7 +8,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if Input.is_action_just_pressed("pause"):
+		GlobalState.paused = false
+		self.queue_free()
 
 
 func _on_button_resume_pressed():
