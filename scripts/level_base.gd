f4d9c3df359bece51705cd3bf45a87bb96b9a60d
nwright <nathanwrightbusiness@gmail.com>
2023-11-22 12:56:53 -0500

    devel

--- scripts/level_base.gd
+++ scripts/level_base.gd
@@ -9,12 +9,30 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if !GlobalState.paused:
-		if Input.is_action_just_pressed("pause"):
-			GlobalState.paused = true
-			var child = preload("res://scenes/levels/pause_menu.tscn").instantiate()
-			child.position = self.get_window().content_scale_size / 2
-			self.add_child(child)
+	if GlobalState.paused:
+		return
+	if Input.is_action_just_pressed("pause"):
+		GlobalState.paused = true
+		var child = preload("res://scenes/levels/pause_menu.tscn").instantiate()
+		child.position = self.get_window().content_scale_size / 2
+		self.add_child(child)
+		return
+	
+	var dir = $Player.get_last_motion()
+	var i = 0
+	if dir.x > 0:
+		i = 1
+	elif dir.x < 0:
+		i = -1
+	
+	i = 0
+	$Placement.position.x = snapped(floor($Player.position.x), 32) + i * 32
+	if dir.y > 0:
+		i = 1
+	elif dir.y < 0:
+		i = -1
+	$Placement.position.y = snapped(floor($Player.position.y), 32) + i * 32
+	
 
 func _on_level_solved():
 	GlobalState.paused = true
@@ -22,7 +40,6 @@ func _on_level_solved():
 	child.position = self.get_window().content_scale_size / 2
 	child.next_level = next_level
 	self.add_child(child)
-
 
 func _on_lamp_logic_changed(state: bool, _id: int):
 	if state:
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -11,7 +11,7 @@ func _process(_delta):
 
 
 func _on_play_button_pressed():
-	get_tree().change_scene_to_packed(load("res://scenes/levels/level_test.tscn"))
+	get_tree().change_scene_to_packed(load("res://scenes/levels/level1.tscn"))
 
 func _on_level_select_button_pressed():
 	get_tree().change_scene_to_packed(preload("res://scenes/levels/level_select.tscn"))
