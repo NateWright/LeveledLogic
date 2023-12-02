142cef7040838e994d7a3d5e6ee2a39bb5c99dfc
nwright <nathanwrightbusiness@gmail.com>
2023-12-01 20:04:21 -0500

    Prettied up credits

--- scripts/credits_menu.gd
+++ scripts/credits_menu.gd
@@ -3,16 +3,12 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var website_button = Button.new()
-	website_button.text = "Leveled Logic Website"
-	$MarginContainer/CenterContainer/VBoxContainer.add_child(website_button)
-	website_button.pressed.connect(_on_button_website_pressed)
-	add_names_label()
+	pass
+#	add_names_label()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
-
 
 func _on_button_main_menu_pressed():
 	get_tree().change_scene_to_packed(load("res://scenes/main_menu/main_menu.tscn"))
@@ -23,10 +19,10 @@ func _on_button_website_pressed():
 	OS.shell_open(url)
 
 
-func add_names_label():
-	var names_label = Label.new()
-	var developers = ["Johnathan Kang", "Connor Klein", "Gabriel Shahrouzi", "Eric Ta", "Nathan Wright"]
-	names_label.text = "Developed by: "
-	for dev in developers:
-		names_label.text += dev + ', '
-	$MarginContainer/CenterContainer/VBoxContainer.add_child(names_label)
+#func add_names_label():
+#	var names_label = Label.new()
+#	var developers = ["Johnathan Kang", "Connor Klein", "Gabriel Shahrouzi", "Eric Ta", "Nathan Wright"]
+#	names_label.text = "Developed by: "
+#	for dev in developers:
+#		names_label.text += dev + ', '
+#	$MarginContainer/CenterContainer/VBoxContainer.add_child(names_label)
