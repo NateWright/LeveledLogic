912b8d46f83740adf2bd5bd6c088cc7d68d25845
nwright <nathanwrightbusiness@gmail.com>
2023-12-02 15:13:08 -0500

    Added tutorial to levels

--- scripts/global_state.gd
+++ scripts/global_state.gd
@@ -5,7 +5,7 @@ var paused = false;
 const gridSize = 64
 
 const LEVELS = [
-	["res://scenes/levels/level1.tscn", "Level 1"],
+	["res://scenes/levels/tutorial_and.tscn", "Tutorial 1: AND"],
 	["res://scenes/main_menu/main_menu.tscn", "Back to Main Menu"]
 ]
 var curLevel: int = 0
