a4422da594162e8d6c6489a32cf26724248ce95b
EricWildArchive <eta2149@gmail.com>
2023-12-04 12:26:15 -0500

    added tutorial for or gate

---
+++ scenes/levels/tutorial_or.gd
@@ -0,0 +1,26 @@
+extends "res://scripts/level_base.gd"
+
+var time = 0
+
+var index = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super._ready()
+	providedInput = [[true, true],[true,false],[false,false],[false,true]]
+	expectedOutput = [[true], [true], [false], [true]]
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	super._process(delta)
+	
+	time += delta
+	
+	if time > 1:
+		time -= 1
+		getSource(0)._setOutput(providedInput[index][0], randi())
+		getSource(1)._setOutput(providedInput[index][1], randi())
+		index += 1
+		index = index % providedInput.size()
+		
--- scripts/global_state.gd
+++ scripts/global_state.gd
@@ -5,6 +5,8 @@ var paused = false;
 const gridSize = 64
 
 const LEVELS = [
+	["res://scenes/levels/tutorial_wire.tscn", "Tutorial 0: Wires"],
 	["res://scenes/levels/tutorial_and.tscn", "Tutorial 1: AND"],
+	["res://scenes/levels/tutorial_or.tscn", "Tutorial 2: OR"],
 ]
 var curLevel: int = 0
