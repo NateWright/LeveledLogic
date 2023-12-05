2f4d4651a09815abe0195c9cfd12f6036e523ce2
EricWildArchive <eta2149@gmail.com>
2023-12-05 11:03:49 -0500

    added not gate tutorial

---
+++ scenes/levels/tutorial_not.gd
@@ -0,0 +1,8 @@
+extends BaseLevel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	super._ready()
+	providedInput = [[true], [false]]
+	expectedOutput = [[false], [true]]
