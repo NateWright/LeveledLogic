a69dab2293b904a203ae77672d82c1b2d42ec372
Shahrouzi <gabriel_shahrouzi@student.uml.edu>
2023-12-06 00:05:29 -0500

    Add the remaining tutorials

--- scripts/global_state.gd
+++ scripts/global_state.gd
@@ -8,5 +8,10 @@ const LEVELS = [
 	["res://scenes/levels/tutorial_wire.tscn", "Tutorial 0: Wires"],
 	["res://scenes/levels/tutorial_and.tscn", "Tutorial 1: AND"],
 	["res://scenes/levels/tutorial_or.tscn", "Tutorial 2: OR"],
+	["res://scenes/levels/tutorial_nand.tscn", "Tutorial 3: NAND"],
+	["res://scenes/levels/tutorial_nor.tscn", "Tutorial 4: NOR"],
+	["res://scenes/levels/tutorial_xor.tscn", "Tutorial 5: XOR"],
+	["res://scenes/levels/tutorial_xnor.tscn", "Tutorial 6: XNOR"]
+	
 ]
 var curLevel: int = 0
