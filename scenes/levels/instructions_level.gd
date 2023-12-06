3c4c2b9a0351714acd85914a780173e73686164e
EricWildArchive <eta2149@gmail.com>
2023-12-06 12:50:44 -0500

    added instructions menu

---
+++ scenes/levels/instructions_level.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
+
+func _on_button_pressed():
+	GlobalState.paused = false
+	self.queue_free()
