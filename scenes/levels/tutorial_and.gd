d11437152c197911b1ae51f7627841a56a61877d
EricWildArchive <eta2149@gmail.com>
2023-12-06 19:43:31 -0500

    Tutorial_and comments

--- scenes/levels/tutorial_and.gd
+++ scenes/levels/tutorial_and.gd
@@ -5,3 +5,8 @@ func _ready():
 	super._ready()
 	providedInput = [[true, true],[true,false],[false,false],[false,true]]
 	expectedOutput = [[true], [false], [false], [false]]
+# This level is designed for you to be familiar with AND Gates
+# While in gate tool hotbar, Press 2 to select the AND gate
+# Now press E to place the gate onto the cursor.
+# The AND gate will not turn on unless both of its inputs are also on.
+
