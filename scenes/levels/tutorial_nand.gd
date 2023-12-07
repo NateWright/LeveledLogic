dcb5907abe6d991c8ea6836743740d7ddc6336aa
connor <47131276+Yodude2002@users.noreply.github.com>
2023-12-06 23:22:35 -0500

    fixed nand

--- scenes/levels/tutorial_nand.gd
+++ scenes/levels/tutorial_nand.gd
@@ -4,4 +4,4 @@ extends BaseLevel
 func _ready():
 	super._ready()
 	providedInput = [[true, true],[true,false],[false,false],[false,true]]
-	expectedOutput = [[false], [false], [false], [true]]
+	expectedOutput = [[false], [true], [true], [true]]
