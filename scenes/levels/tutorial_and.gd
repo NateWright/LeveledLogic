8829b488cfa0708d00b704a53858c32d8b57aab0
EricWildArchive <eta2149@gmail.com>
2023-12-06 20:01:05 -0500

    Made more comments

--- scenes/levels/tutorial_and.gd
+++ scenes/levels/tutorial_and.gd
@@ -5,8 +5,11 @@ func _ready():
 	super._ready()
 	providedInput = [[true, true],[true,false],[false,false],[false,true]]
 	expectedOutput = [[true], [false], [false], [false]]
+# Wire the AND gate and match the expected output to complete the level
 # This level is designed for you to be familiar with AND Gates
-# While in gate tool hotbar, Press 2 to select the AND gate
+# While in gate tool hotbar, Press 3 to select the AND gate
 # Now press E to place the gate onto the cursor.
+# The AND gate accepts two inputs and makes one output
 # The AND gate will not turn on unless both of its inputs are also on.
+# Wire the AND gate from both source blocks and into the sink block
 
--- scenes/levels/tutorial_not.gd
+++ scenes/levels/tutorial_not.gd
@@ -6,3 +6,11 @@ func _ready():
 	super._ready()
 	providedInput = [[true], [false]]
 	expectedOutput = [[false], [true]]
+
+# Wire the NOT gate and match the expected output to finish the level
+# Press 2 to select the NOT gate
+# Press E to place the gate onto the cursor
+# Now, by pressing TAB, you will go into the wire hotbar, with wire input being default
+# you can press E to make a connection on a source block
+# you can complete the connection by press 2 to select wire output and then
+# pressing E on what you want to connect to.
