9eac2b7b176fd377d35b79f87cdf39f096c5d5ae
nwright <nathanwrightbusiness@gmail.com>
2023-12-07 19:47:18 -0500

    moved tutorials to sub folder

--- scripts/global_state.gd
+++ scripts/global_state.gd
@@ -5,14 +5,14 @@ var paused = false;
 const gridSize = 64
 
 const LEVELS = [
-	["res://scenes/levels/tutorial_wire.tscn", "Tutorial 0: Wires"],
-	["res://scenes/levels/tutorial_not.tscn", "Tutorial 1: NOT"],
-	["res://scenes/levels/tutorial_and.tscn", "Tutorial 2: AND"],
-	["res://scenes/levels/tutorial_or.tscn", "Tutorial 3: OR"],
-	["res://scenes/levels/tutorial_xor.tscn", "Tutorial 4: XOR"],
-	["res://scenes/levels/tutorial_nand.tscn", "Tutorial 5: NAND"],
-	["res://scenes/levels/tutorial_nor.tscn", "Tutorial 6: NOR"],
-	["res://scenes/levels/tutorial_xnor.tscn", "Tutorial 7: XNOR"]
+	["res://scenes/levels/tutorials/tutorial_wire.tscn", "Tutorial 0: Wires"],
+	["res://scenes/levels/tutorials/tutorial_not.tscn", "Tutorial 1: NOT"],
+	["res://scenes/levels/tutorials/tutorial_and.tscn", "Tutorial 2: AND"],
+	["res://scenes/levels/tutorials/tutorial_or.tscn", "Tutorial 3: OR"],
+	["res://scenes/levels/tutorials/tutorial_xor.tscn", "Tutorial 4: XOR"],
+	["res://scenes/levels/tutorials/tutorial_nand.tscn", "Tutorial 5: NAND"],
+	["res://scenes/levels/tutorials/tutorial_nor.tscn", "Tutorial 6: NOR"],
+	["res://scenes/levels/tutorials/tutorial_xnor.tscn", "Tutorial 7: XNOR"]
 	
 ]
 var curLevel: int = 0
